24ef63b1756caa248147b45cb9805308b83327c0
marclura <marco@marcolurati.ch>
2017-11-17 17:13:33 +0100

    horizontal images for news updated

--- _components/imgBox/index.rb
+++ _components/imgBox/index.rb
@@ -9,11 +9,17 @@ class ImgBoxController
 			end
 		end
 
-		if !File.exists?(data['image'])
-			puts "Image doesn't exist: #{data['image']}"
-			data.delete 'image'
+		if !File.exists?(data['image'].sub! '.jpg', '_h.jpg')
+			puts "Horizontal image doesn't exist: #{data['image']}"
+			if !File.exists?(data['image'].sub! '_h.jpg', '.jpg')
+				puts "Image doesn't exist: #{data['image']}"
+				data.delete 'image'
+			end
 		end
 
+		puts data['image']
+
 		return data
+
 	end
 end
